d7616bdfc7b707beaa94a6d157c9475e27d6e2ed
Christopher Leggett <chris@leggett.dev>
2021-06-01 00:31:38 -0400

    Generates model from sql files (schema and query).

--- api/go.mod
+++ api/go.mod
@@ -3,11 +3,15 @@ module github.com/leggettc18/grindlists/api
 go 1.16
 
 require (
+	github.com/99designs/gqlgen v0.13.0 // indirect
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/graph-gophers/graphql-go v1.1.0
+	github.com/kyleconroy/sqlc v1.8.0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/rs/zerolog v1.22.0
 	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/spf13/viper v1.7.1 // indirect
+	github.com/vektah/gqlparser v1.2.0 // indirect
 )
---
+++ api/query.sql
@@ -0,0 +1,79 @@
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users ORDER BY name;
+
+-- name: CreateUser :one
+INSERT INTO users (name, email, hashed_password, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $4)
+RETURNING *;
+
+-- name: UpdateUser :one
+UPDATE users
+SET name = $2, email= $3, hashed_password = $4, updated_at = $5
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :one
+DELETE FROM users
+WHERE id = $1
+RETURNING *;
+
+-- name: GetList :one
+SELECT * FROM lists WHERE id = $1;
+
+-- name: ListLists :many
+SELECT * FROM lists ORDER BY name;
+
+-- name: CreateList :one
+INSERT INTO lists (name, user_id, created_at, updated_at)
+VALUES ($1, $2, $3, $3)
+RETURNING *;
+
+-- name: UpdateList :one
+UPDATE lists
+SET name = $2, user_id = $3, updated_at = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteList :one
+DELETE FROM lists
+WHERE id = $1
+RETURNING *;
+
+-- name: GetItem :one
+SELECT * FROM items WHERE id = $1;
+
+-- name: ListItems :many
+SELECT * FROM items ORDER BY name;
+
+-- name: CreateItem :one
+INSERT INTO items (name, source, created_at, updated_at)
+VALUES ($1, $2, $3, $3)
+RETURNING *;
+
+-- name: UpdateItem :one
+UPDATE items
+SET name = $2, source = $3, updated_at = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteItem :one
+DELETE FROM items
+WHERE id = $1
+RETURNING *;
+
+-- name: SetListItem :one
+INSERT INTO list_items (quantity, collected, list_id, item_id, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $5, $5)
+RETURNING *;
+
+-- name: UpdateListItem :exec
+UPDATE list_items
+SET quantity = $2, collected = $3, updated_at = $4
+WHERE id = $1;
+
+-- name: UnsetListItem :exec
+DELETE FROM list_items
+WHERE id = $1;
