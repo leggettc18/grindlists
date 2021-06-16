40dd75a2b3e3295ea3af48899972010b44c0382e
Christopher Leggett <chris@leggett.dev>
2021-06-16 10:44:06 -0400

    Implments cache update after login.

--- web/components/NavBar.tsx
+++ web/components/NavBar.tsx
@@ -1,8 +1,39 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import Link from "next/link";
+import { MeQuery, useMeQuery } from "../generated/graphql";
+import { ApolloError } from "@apollo/client";
+import ClientOnly from "./ClientOnly";
 
 export default function NavBar() {
+  const { data, loading, error, refetch } = useMeQuery();
+  let body = null;
+
+  if (!loading && !error) {
+    body = (
+      <Link href="/logout">
+        <a className="hover:bg-sunset-600 text-sunset-100 bg-sunset-500 border border-sunset-600 rounded-lg p-1 shadow-xl">
+          Logout
+        </a>
+      </Link>
+    );
+  } else {
+    body = (
+      <>
+        <Link href="/login">
+          <a className="bg-olive-300 text-olive-700 p-1 shadow-xl border-olive-400 border rounded-lg">
+            Login
+          </a>
+        </Link>
+        <Link href="/register">
+          <a className="bg-seagreen-300 text-seagreen-700 p-1 shadow-xl border-seagreen-400 border rounded-lg">
+            Register
+          </a>
+        </Link>
+      </>
+    );
+  }
+
   return (
     <div>
       <nav className="bg-steel-500">
@@ -13,10 +44,7 @@ export default function NavBar() {
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <Link href="/">
-                    <a
-                      href="#"
-                      className="hover:bg-steel-700 text-gray-100 px-3 py-2 rounded-md text-sm font-medium"
-                    >
+                    <a className="hover:bg-steel-700 text-gray-100 px-3 py-2 rounded-md text-sm font-medium">
                       Dashboard
                     </a>
                   </Link>
@@ -29,16 +57,7 @@ export default function NavBar() {
               </div>
             </div>
             <div className="flex space-x-2">
-              <Link href="/login">
-                <a className="bg-olive-300 text-olive-700 p-1 shadow-xl border-olive-400 border rounded-lg">
-                  Login
-                </a>
-              </Link>
-              <Link href="/register">
-                <a className="bg-seagreen-300 text-seagreen-700 p-1 shadow-xl border-seagreen-400 border rounded-lg">
-                  Register
-                </a>
-              </Link>
+              <ClientOnly>{body}</ClientOnly>
             </div>
           </div>
         </div>
--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,28 +1,47 @@
+import { gql } from "@apollo/client";
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
-import { LoginInput, useLoginMutation } from "../generated/graphql";
+import {
+  LoginInput,
+  MeDocument,
+  MeQuery,
+  useLoginMutation,
+  useMeQuery,
+} from "../generated/graphql";
 import { useForm } from "../hooks/useForm";
 
 export default function Login() {
-    const initialState = {
-      email: "",
-      password: "",
-    };
+  const initialState = {
+    email: "",
+    password: "",
+  };
 
-    const { onChange, onSubmit, values } = useForm<LoginInput>(handleLogin, initialState);
+  const { onChange, onSubmit, values } = useForm<LoginInput>(
+    handleLogin,
+    initialState
+  );
 
-    const [login, {data, loading, error}] = useLoginMutation();
+  const [login, { data, loading, error }] = useLoginMutation({
+    update(cache, { data }) {
+      cache.writeQuery({
+        query: MeDocument,
+        data: {
+          me: data?.login
+        },
+      });
+    },
+  });
 
-    const router = useRouter();
+  const router = useRouter();
 
-    async function handleLogin() {
-        const response = await login({variables: {data: values}});
-        if (response.errors) {
-            console.error(error)
-        } else {
-            router.push("/");
-        }
+  async function handleLogin() {
+    const response = await login({ variables: { data: values } });
+    if (response.errors) {
+      console.error(error);
+    } else {
+      router.push("/");
     }
+  }
 
   return (
     <div className="flex flex-col items-center space-y-4 pt-4">
@@ -54,7 +73,12 @@ export default function Login() {
             onChange={onChange}
           />
         </div>
-        <button type="submit" className="p-2 border border-steel-700 bg-steel-500 text-gray-100">Login</button>
+        <button
+          type="submit"
+          className="p-2 border border-steel-700 bg-steel-500 text-gray-100"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
