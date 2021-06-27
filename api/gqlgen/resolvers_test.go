1220bb41a87fc79a2dacd2597fa10b1208c07e99
Christopher Leggett <chris@leggett.dev>
2021-06-27 16:52:55 -0400

    Improves test coverage.

--- api/gqlgen/resolvers_test.go
+++ api/gqlgen/resolvers_test.go
@@ -2,11 +2,11 @@ package gqlgen
 
 import (
 	"database/sql"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/99designs/gqlgen/client"
-	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/leggettc18/grindlists/api/app"
 	authMocks "github.com/leggettc18/grindlists/api/mocks/auth"
 	repoMocks "github.com/leggettc18/grindlists/api/mocks/pg"
@@ -16,18 +16,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateList(t *testing.T) {
+func TestCreateList_Success(t *testing.T) {
 	auth := new(authMocks.AuthService)
 	repo := new(repoMocks.Repository)
 	app, err := app.New()
 	assert.Nil(t, err)
-	c := client.New(handler.NewDefaultServer(NewExecutableSchema(Config{
-		Resolvers: &Resolver{
-			Repository: repo,
-			App:        *app,
-			Auth:       auth,
-		},
-	})))
+	c := client.New(NewHandler(repo, *app, auth))
 	list := pg.List{
 		ID:        1,
 		Name:      "Test",
@@ -56,4 +50,76 @@ func TestCreateList(t *testing.T) {
 	c.MustPost(query, &response, client.Var("input", ListInput{Name: list.Name}))
 	require.Equal(t, int64(1), response.CreateList.ID)
 	require.Equal(t, "Test", response.CreateList.Name)
+}
+func TestCreateList_Error_NotAuthenticated(t *testing.T) {
+	auth := new(authMocks.AuthService)
+	repo := new(repoMocks.Repository)
+	app, err := app.New()
+	assert.Nil(t, err)
+	c := client.New(NewHandler(repo, *app, auth))
+	list := pg.List{
+		ID:        1,
+		Name:      "Test",
+		UserID:    1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: sql.NullTime{Valid: false},
+	}
+	userID := int64(-1)
+	auth.On("GetUserID", mock.Anything).Return(userID, errors.New("not authenticated"))
+	repo.On("CreateList", mock.Anything, mock.AnythingOfType("pg.CreateListParams")).Return(list, nil)
+	var response struct {
+		CreateList *struct {
+			ID   int64
+			Name string
+		}
+	}
+	query := `
+		mutation CreateList($input: ListInput!) {
+			createList(data: $input) {
+				id
+				name
+			}
+		}
+	`
+	err = c.Post(query, &response, client.Var("input", ListInput{Name: list.Name}))
+	require.Nil(t, response.CreateList)
+	require.EqualError(t, err, "[{\"message\":\"not authenticated\",\"path\":[\"createList\"]}]")
+}
+
+func TestCreateList_Error_Database(t *testing.T) {
+	auth := new(authMocks.AuthService)
+	repo := new(repoMocks.Repository)
+	app, err := app.New()
+	assert.Nil(t, err)
+	c := client.New(NewHandler(repo, *app, auth))
+	list := pg.List{
+		ID:        1,
+		Name:      "Test",
+		UserID:    1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: sql.NullTime{Valid: false},
+	}
+	userID := int64(1)
+	db_error := errors.New("db_error")
+	auth.On("GetUserID", mock.Anything).Return(userID, nil)
+	repo.On("CreateList", mock.Anything, mock.AnythingOfType("pg.CreateListParams")).Return(list, db_error)
+	var response struct {
+		CreateList *struct {
+			ID   int64
+			Name string
+		}
+	}
+	query := `
+		mutation CreateList($input: ListInput!) {
+			createList(data: $input) {
+				id
+				name
+			}
+		}
+	`
+	err = c.Post(query, &response, client.Var("input", ListInput{Name: list.Name}))
+	require.Nil(t, response.CreateList)
+	require.Error(t, err)
 }
