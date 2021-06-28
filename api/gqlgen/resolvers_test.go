47fb210b5dfbdb4514e6c6dde16bb768c2fe443d
Christopher Leggett <chris@leggett.dev>
2021-06-27 22:22:13 -0400

    Completes test for list item resolvers.
    Technically there's a line of code uncovered, but upon further consideration the method that line is in will likely be removed. I haven't done it yet because it will require editing the schema, regenerating the resolver code, remove the resolver method, and then editing some tests to compensate.

--- api/gqlgen/resolvers_test.go
+++ api/gqlgen/resolvers_test.go
@@ -31,8 +31,8 @@ func TestCreateList_Success(t *testing.T) {
 		DeletedAt: sql.NullTime{Valid: false},
 	}
 	user := pg.User{
-		ID: 1,
-		Name: "Test",
+		ID:    1,
+		Name:  "Test",
 		Email: "test@example.com",
 	}
 	userID := int64(1)
@@ -137,7 +137,7 @@ func TestCreateList_Error_Database(t *testing.T) {
 	require.Error(t, err)
 }
 
-func TestGetList_Error_NoUser (t *testing.T) {
+func TestGetList_Error_NoUser(t *testing.T) {
 	auth := new(authMocks.AuthService)
 	repo := new(repoMocks.Repository)
 	app, err := app.New()
@@ -152,8 +152,8 @@ func TestGetList_Error_NoUser (t *testing.T) {
 		DeletedAt: sql.NullTime{Valid: false},
 	}
 	user := pg.User{
-		ID: 1,
-		Name: "Test",
+		ID:    1,
+		Name:  "Test",
 		Email: "test@example.com",
 	}
 	repo.On("GetList", mock.Anything, mock.AnythingOfType("int64")).Return(list, nil)
@@ -179,7 +179,7 @@ func TestGetList_Error_NoUser (t *testing.T) {
 			}
 		}
 	`
-	err = c.Post(query, response, client.Var("input", int64(1)))
+	err = c.Post(query, &response, client.Var("input", int64(1)))
 	require.Nil(t, response.List)
 	require.Error(t, err)
 }
@@ -220,7 +220,7 @@ func TestGetList_Error_NoList(t *testing.T) {
 			}
 		}
 	`
-	err = c.Post(query, response, client.Var("input", int64(1)))
+	err = c.Post(query, &response, client.Var("input", int64(1)))
 	require.Nil(t, response.List)
 	require.Error(t, err)
 }
@@ -240,27 +240,27 @@ func TestGetList_Success(t *testing.T) {
 		DeletedAt: sql.NullTime{Valid: false},
 	}
 	item := pg.Item{
-		ID: 1,
-		Name: "Test",
+		ID:     1,
+		Name:   "Test",
 		Source: "Test",
 	}
 	listItem1 := pg.ListItem{
-		ID: 1,
-		Quantity: sql.NullInt64{Valid: false},
+		ID:        1,
+		Quantity:  sql.NullInt64{Valid: false},
 		Collected: false,
-		ItemID: item.ID,
-		ListID: 1,
+		ItemID:    item.ID,
+		ListID:    1,
 	}
 	listItem2 := pg.ListItem{
-		ID: 2,
-		Quantity: sql.NullInt64{Int64: 1, Valid: true},
+		ID:        2,
+		Quantity:  sql.NullInt64{Int64: 1, Valid: true},
 		Collected: false,
-		ItemID: item.ID,
-		ListID: 1,
+		ItemID:    item.ID,
+		ListID:    1,
 	}
 	user := pg.User{
-		ID: 1,
-		Name: "Test",
+		ID:    1,
+		Name:  "Test",
 		Email: "test@example.com",
 	}
 	repo.On("GetList", mock.Anything, mock.AnythingOfType("int64")).Return(list, nil)
@@ -269,15 +269,15 @@ func TestGetList_Success(t *testing.T) {
 	repo.On("GetItem", mock.Anything, mock.AnythingOfType("int64")).Return(item, nil)
 	var response struct {
 		List *struct {
-			ID   int64
-			Name string
-			User pg.User
+			ID    int64
+			Name  string
+			User  pg.User
 			Items []*struct {
-				ID int64
-				Quantity *int64
+				ID        int64
+				Quantity  *int64
 				Collected bool
-				Item pg.Item
-				List pg.List
+				Item      pg.Item
+				List      pg.List
 			}
 		}
 	}
@@ -317,4 +317,156 @@ func TestGetList_Success(t *testing.T) {
 	require.Equal(t, *listItems[1].Quantity, int64(1))
 	require.Equal(t, listItems[0].Item.ID, int64(1))
 	require.Equal(t, listItems[0].List.Name, "Test")
+}
+
+func TestGetListListItems_Error_Database(t *testing.T) {
+	auth := new(authMocks.AuthService)
+	repo := new(repoMocks.Repository)
+	app, err := app.New()
+	assert.Nil(t, err)
+	c := client.New(NewHandler(repo, *app, auth))
+	list := pg.List{
+		ID:        1,
+		Name:      "Test",
+		UserID:    1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: sql.NullTime{Valid: false},
+	}
+	db_error := errors.New("db_error")
+	repo.On("GetList", mock.Anything, mock.AnythingOfType("int64")).Return(list, nil)
+	repo.On("GetListListItems", mock.Anything, mock.AnythingOfType("int64")).Return([]pg.ListItem{}, db_error)
+	var response struct {
+		List *struct {
+			ID    int64
+			Name  string
+			Items *[]pg.ListItem
+		}
+	}
+	query := `
+		query List($input: ID!) {
+			list(id: $input) {
+				id
+				name
+				items {
+					id
+					quantity
+					collected
+				}
+			}
+		}
+	`
+	err = c.Post(query, &response, client.Var("input", int64(1)))
+	require.Nil(t, response.List.Items)
+	require.Error(t, err)
+}
+
+func TestGetListListItems_NoListItems(t *testing.T) {
+	auth := new(authMocks.AuthService)
+	repo := new(repoMocks.Repository)
+	app, err := app.New()
+	assert.Nil(t, err)
+	c := client.New(NewHandler(repo, *app, auth))
+	list := pg.List{
+		ID:        1,
+		Name:      "Test",
+		UserID:    1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: sql.NullTime{Valid: false},
+	}
+	repo.On("GetList", mock.Anything, mock.AnythingOfType("int64")).Return(list, nil)
+	repo.On("GetListListItems", mock.Anything, mock.AnythingOfType("int64")).Return([]pg.ListItem{}, nil)
+	var response struct {
+		List *struct {
+			ID    int64
+			Name  string
+			Items []*struct {
+				ID        int64
+				Quantity  *int64
+				Collected bool
+			}
+		}
+	}
+	query := `
+		query List($input: ID!) {
+			list(id: $input) {
+				id
+				name
+				items {
+					id
+					quantity
+					collected
+				}
+			}
+		}
+	`
+	err = c.Post(query, &response, client.Var("input", int64(1)))
+	require.Empty(t, response.List.Items)
+	require.Nil(t, err)
+}
+
+func TestGetListItemItem_Error_Database(t *testing.T) {
+	auth := new(authMocks.AuthService)
+	repo := new(repoMocks.Repository)
+	app, err := app.New()
+	assert.Nil(t, err)
+	c := client.New(NewHandler(repo, *app, auth))
+	list := pg.List{
+		ID:        1,
+		Name:      "Test",
+		UserID:    1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: sql.NullTime{Valid: false},
+	}
+	item := pg.Item{
+		ID:     1,
+		Name:   "Test",
+		Source: "Test",
+	}
+	listItem := pg.ListItem{
+		ID:        1,
+		Quantity:  sql.NullInt64{Valid: false},
+		Collected: false,
+		ItemID:    item.ID,
+		ListID:    1,
+	}
+	item_error := errors.New("item error")
+	repo.On("GetList", mock.Anything, mock.AnythingOfType("int64")).Return(list, nil)
+	repo.On("GetListListItems", mock.Anything, mock.AnythingOfType("int64")).Return([]pg.ListItem{listItem}, nil)
+	repo.On("GetItem", mock.Anything, mock.AnythingOfType("int64")).Return(item, item_error)
+	var response struct {
+		List *struct {
+			ID    int64
+			Name  string
+			Items []*struct {
+				ID        int64
+				Quantity  *int64
+				Collected bool
+				Item      pg.Item
+			}
+		}
+	}
+	query := `
+		query List($input: ID!) {
+			list(id: $input) {
+				id
+				name
+				items {
+					id
+					quantity
+					collected
+					item {
+						id
+						name
+						source
+					}
+				}
+			}
+		}
+	`
+	err = c.Post(query, &response, client.Var("input", int64(1)))
+	require.Error(t, err)
+	require.Nil(t, response.List.Items[0])
 }
